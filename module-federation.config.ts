d82a451e0770b61011bd51aea428c4016ce2aef1
IsKJH <dkdlrh147@gmail.com>
2025-08-07 15:44:20 +0900

    테스트

---
+++ module-federation.config.ts
@@ -0,0 +1,8 @@
+export const mfConfig = {
+    name: "react_s3_test",
+    remotes: {},
+    shared: {
+        react: { singleton: true, requiredVersion: "^18.2.0", eager: true, },
+        "react-dom": { singleton: true, requiredVersion: "^18.2.0", eager: true, },
+    },
+};
